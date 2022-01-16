11962e8b11fcbca5a5c547ddb8733a1d52d201d2
Andrey Martianov <martianovas@gmail.com>
2022-01-16 19:53:52 +0700

    Add initial netty server

---
+++ src/main/java/com/github/martianov/le/LeaderElectionApp.java
@@ -0,0 +1,55 @@
+package com.github.martianov.le;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+public class LeaderElectionApp {
+    public void start(int port) throws Exception {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) {
+                        }
+                    })
+                    .option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+
+            ChannelFuture f = b.bind(port).sync();
+
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+
+    public static int DEFAULT_PORT = 8092;
+
+    private static int getPort() {
+        String portString = System.getenv("PORT");
+        if (portString == null) {
+            return DEFAULT_PORT;
+        }
+        return Integer.parseInt(portString, 10);
+    }
+
+    public static void main(String[] args) throws Exception {
+        int portString = getPort();
+
+        new LeaderElectionApp().start(portString);
+    }
+}
